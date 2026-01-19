f0a81cf50b4d07f154685bdc2abc1ac458dfdea3
Lukas Ramos <lukas.deltaflex@gmail.com>
2026-01-19 18:36:20 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:341426752875:web:348f88597e5b9b2057d02e",
   "apiKey": "AIzaSyDcdnNBy0TZTsq_cI02KFVU9o7PJopEczM",
   "authDomain": "studio-248448941-9c1c2.firebaseapp.com",
+  "storageBucket": "studio-248448941-9c1c2.appspot.com",
   "measurementId": "G-TX4KQ9R6TL",
   "messagingSenderId": "341426752875"
 };
