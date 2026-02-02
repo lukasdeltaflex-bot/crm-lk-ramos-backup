31c196616ce6e82e74fabf0db323dd458182a092
Lukas Ramos <lukas.deltaflex@gmail.com>
2026-02-02 21:26:41 +0000

    ## Error Type Runtime Error
    ## Error Message
    FIRESTORE (11.9.0) INTERNA

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getAuth, Auth } from "firebase/auth";
-import { getFirestore, Firestore, initializeFirestore, CACHE_SIZE_UNLIMITED, terminate } from "firebase/firestore";
+import { getFirestore, Firestore, initializeFirestore, CACHE_SIZE_UNLIMITED } from "firebase/firestore";
 import { getStorage, FirebaseStorage } from "firebase/storage";
 
 const firebaseConfig = {
